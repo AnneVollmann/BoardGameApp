bb7da2cf42df8df5eca14051c4a4fbac026647aa
Anne Vollmann <vollmann.anne@web.de>
2026-08-12 09:44:39 +0200

    basic Routing

--- BoardGameApp/src/app/app.routes.ts
+++ BoardGameApp/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { TuringMachine } from './games/turing-machine/turing-machine';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'games/turing-machine',
+    component: TuringMachine,
+  },
+];
