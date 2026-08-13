7071e4609a9e13c46f717d21fc4ee177c3febbc0
Anne Vollmann <vollmann.anne@web.de>
2026-08-13 09:25:18 +0200

    style: tm text color

--- BoardGameApp/src/app/games/turing-machine/code-table/code-table.ts
+++ BoardGameApp/src/app/games/turing-machine/code-table/code-table.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-code-table',
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './code-table.scss',
 })
 export class CodeTable {
+  @ViewChildren('codeDigit')
+  codeDigits!: QueryList<ElementRef<HTMLInputElement>>;
+
+  onDigitInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    input.value = input.value.replace(/\D/g, '').slice(0, 1);
+    if (input.value.length !== 1) return;
+    const inputs = this.codeDigits.toArray();
+    const currentIndex = inputs.findIndex((item) => item.nativeElement === input,);
+    if ((currentIndex + 1) % 3 === 0) {
+      input.blur();
+      return;
+    }
+    inputs[currentIndex + 1].nativeElement.focus();
+  }
 }
