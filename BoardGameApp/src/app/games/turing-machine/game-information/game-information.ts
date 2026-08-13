5f9f9bc44e5def4f396a3fcb00b27e5129c649bf
Anne Vollmann <vollmann.anne@web.de>
2026-08-13 11:12:16 +0200

    feat: tm game information state management

--- BoardGameApp/src/app/games/turing-machine/game-information/game-information.ts
+++ BoardGameApp/src/app/games/turing-machine/game-information/game-information.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-game-information',
@@ -7,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './game-information.scss',
 })
 export class GameInformation {
+  name = input.required<string>();
+  gameNumber = input.required<string>();
 
+  nameChange = output<string>();
+  gameNumberChange = output<string>();
+
+  onNameInput(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.nameChange.emit(value);
+  }
+
+  onGameNumberInput(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.gameNumberChange.emit(value);
+  }
 }
